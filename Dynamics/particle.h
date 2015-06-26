e34dccbe58b3054c2356c1703949d596c8e595c7
rballouz <ronald.ballouz@gmail.com>
2015-06-26 15:24:13 -0400

    Code for Soft-Sphere collisions with static friction

---
+++ Dynamics/particle.h
@@ -0,0 +1,32 @@
+typedef struct particle{
+  int iOrder, iAggOrder, iColor;
+  double dMass;
+  double dRadius;
+  double dKn;
+  double dKt;
+  double dEpsN;
+  double dEpsT;
+  double dMuS;
+  //Vec vNhat; //unit vector of relative velocity between particle and neighbor
+  //Vec vOrient;
+  //Mat mInertia;
+  Vec r;
+  Vec v;
+  Vec w;
+  Vec a;
+  Vec vnOld;
+  Vec vShear;
+} PARTICLE;
+
+
+
+
+
+
+
+
+
+
+
+
+
